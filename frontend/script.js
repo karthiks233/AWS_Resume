cb4af81312f72c99b4eddc76bddbb710242d26ec
Karthik Raja <67165351+karthiks233@users.noreply.github.com>
2025-05-17 16:06:03 -0400

    Update script.js | updated api gateway

--- frontend/script.js
+++ frontend/script.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const visitorCountElement = document.getElementById('visitor-count');
 
-    fetch('https://myp73qqck8.execute-api.us-east-1.amazonaws.com/prod/VisitorCounter')
+    fetch('https://myp73qqck8.execute-api.us-east-1.amazonaws.com/prod/my-resume-counter-stack1-VisitorCounterFunction-3DMBBzQXRNQ4')
         .then(response => response.json())
         .then(data => {
             if (data && data.count !== undefined) {
